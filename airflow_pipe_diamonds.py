dc8df0a4f75199e76793bdfed122681c24cfca7a
Mark Furman <153282856+marcuryyy@users.noreply.github.com>
2025-04-14 10:30:28 +0500

    initial commit

---
+++ airflow_pipe_diamonds.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import timedelta
+from train_model import train
+
+
+def download_dataset():
+    df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/diamonds.csv')
+    df.to_csv('data.csv', index=False)
+    return df
+
+
+def clear_data(path2df='data.csv'):
+    df = pd.read_csv(path2df)
+
+    price_col = df['price']
+    df = df.drop(['price'], axis=1)  # Удалим колонку с ценой, т.к. это наша целевая переменная
+
+    # В колонках пропущенных значений нет, а это значит, можем сразу переходить к кодированию данных
+    cat_cols = df.select_dtypes(include=['object', 'category']).columns
+    num_cols = df.select_dtypes(include=['number']).columns
+
+    # Кодировать будем с помощью OneHotEncoder, дропая первую колонку параметром drop = 'first'
+    ohe = OneHotEncoder(drop='first', sparse_output=False)
+
+    encoded_data = ohe.fit_transform(df[cat_cols])
+    encoded_df = pd.DataFrame(encoded_data, columns=ohe.get_feature_names_out(cat_cols))
+
+    df = df.drop(cat_cols, axis=1)
+    df = pd.concat([df, encoded_df], axis=1)
+
+    # Для колонок, которые содержали числовые значения, применим StandardScaler, чтобы не было больших отличий между
+    # данными (в т.ч. выбросы).
+    scaler = StandardScaler()
+    df[num_cols] = scaler.fit_transform(df[num_cols])
+    df['price'] = price_col # вернем колонку с ценой
+
+    df.to_csv('df_clear.csv')
+    return True
+
+
+dag_diamonds = DAG(
+    dag_id="train_pipeline",
+    start_date=datetime(2025, 2, 3),
+   # concurrency=4, # Убрали в airflow3
+    #schedule_interval=timedelta(minutes=5), # убрали в airflow3
+    schedule="@hourly",
+    max_active_runs=1,
+    catchup=False,
+)
+download_task = PythonOperator(python_callable=download_dataset, task_id="download_diamons_dataset", dag=dag_diamonds)
+clear_task = PythonOperator(python_callable=clear_data, task_id="clear_dataset", dag=dag_diamonds)
+train_task = PythonOperator(python_callable=train, task_id="train_model", dag=dag_diamonds)
+download_task >> clear_task >> train_task
